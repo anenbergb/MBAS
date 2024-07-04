f5c6beb83c75ad7b2f944b9938b83557a5139619
Bryan Anenberg <anenbergb@gmail.com>
2024-07-04 10:37:55 -0700

    first pass at mednext decoder

--- mbas/architectures/MedNeXt.py
+++ mbas/architectures/MedNeXt.py
@@ -448,6 +448,8 @@ class MedNeXtEncoder(nn.Module):
         self.output_channels = features_per_stage
         self.return_skips = return_skips
 
+        self.n_blocks_per_stage = n_blocks_per_stage
+        self.exp_ratio_per_stage = exp_ratio_per_stage
         # we store some things that a potential decoder needs
         self.conv_op = conv_op
         self.kernel_size = kernel_size
@@ -476,17 +478,8 @@ class MedNeXtDecoder(nn.Module):
         encoder: MedNeXtEncoder,
         num_classes: int,
         n_blocks_per_stage: List[int],
+        exp_ratio_per_stage: List[int],
         deep_supervision: bool = True,
-        # input_channels: int,
-        # n_stages: int,
-        # exp_ratios: List[int],
-        # conv_op: Type[_ConvNd],
-        # kernel_size: int = 7,
-        do_res: bool = False,  # Can be used to individually test residual connection
-        do_res_up_down: bool = False,  # Additional 'res' connection on up and down convs
-        norm_type="group",
-        dim="3d",  # 2d or 3d
-        grn=False,
     ):
         """
         This class needs the skips of the encoder as input in its forward.
@@ -498,10 +491,10 @@ class MedNeXtDecoder(nn.Module):
         1) conv transpose to upsample the feature maps of the stage below it (or the bottleneck in case of the first stage)
         2) n_conv_per_stage conv blocks to let the two inputs get to know each other and merge
         3) (optional if deep_supervision=True) a segmentation output Todo: enable upsample logits?
-        :param encoder:
-        :param num_classes:
-        :param n_conv_per_stage:
-        :param deep_supervision:
+
+
+        Possible encoder.output_channels [32, 64, 128, 256, 512]
+            decoder channels [512, 256, 128, 64, 32]
         """
         super().__init__()
         self.deep_supervision = deep_supervision
@@ -509,26 +502,48 @@ class MedNeXtDecoder(nn.Module):
         self.num_classes = num_classes
         n_stages_encoder = len(encoder.output_channels)
 
-        assert len(n_blocks_per_stage) == n_stages
-        assert len(exp_ratios) == n_stages
-
-        assert len(n_conv_per_stage) == n_stages_encoder - 1, (
-            "n_conv_per_stage must have as many entries as we have "
+        assert len(n_blocks_per_stage) == n_stages_encoder - 1, (
+            "n_blocks_per_stage must have as many entries as we have "
             "resolution stages - 1 (n_stages in encoder - 1), "
             "here: %d" % n_stages_encoder
         )
+        assert len(exp_ratio_per_stage) == n_blocks_per_stage
+
+        conv_op = encoder.conv_op
+        kernel_size = encoder.kernel_size
+        do_res = encoder.do_res
+        do_res_up_down = encoder.do_res_up_down
+        norm_type = encoder.norm_type
+        grn = encoder.grn
+        dim = encoder.dim
 
         self.stages = nn.ModuleList()
         self.up_blocks = nn.ModuleList()
         self.seg_layers = nn.ModuleList()
-        for i in range(n_stages):
-            channels = input_channels // (2**i)
+        for s in range(1, n_stages_encoder):
+            input_features_below = encoder.output_channels[-s]
+            input_features_skip = encoder.output_channels[-(s + 1)]
+
+            # we always build the deep supervision outputs so that we can always load parameters. If we don't do this
+            # then a model trained with deep_supervision=True could not easily be loaded at inference time where
+            # deep supervision is not needed. It's just a convenience thing
+            self.seg_layers.append(
+                encoder.conv_op(
+                    input_features_below,
+                    num_classes,
+                    kernel_size=1,
+                    stride=1,
+                    padding=0,
+                    bias=True,
+                )
+            )
+
             self.up_blocks.append(
                 MedNeXtUpBlock(
-                    in_channels=channels,
-                    out_channels=channels // 2,
+                    in_channels=input_features_below,
+                    out_channels=input_features_skip,
                     conv_op=conv_op,
-                    exp_r=exp_ratios[i],
+                    exp_r=exp_ratio_per_stage[s - 1],
                     kernel_size=kernel_size,
                     do_res=do_res_up_down,
                     norm_type=norm_type,
@@ -539,35 +554,59 @@ class MedNeXtDecoder(nn.Module):
             stage = nn.Sequential(
                 *[
                     MedNeXtBlock(
-                        in_channels=channels // 2,
-                        out_channels=channels // 2,
+                        in_channels=input_features_skip,
+                        out_channels=input_features_skip,
                         conv_op=conv_op,
-                        exp_r=exp_ratios[i],
+                        exp_r=exp_ratio_per_stage[s - 1],
                         kernel_size=kernel_size,
                         do_res=do_res,
                         norm_type=norm_type,
                         dim=dim,
                         grn=grn,
                     )
-                    for _ in range(block_counts[i])
+                    for _ in range(n_blocks_per_stage[s - 1])
                 ]
             )
             self.stages.append(stage)
-            # we always build the deep supervision outputs so that we can always load parameters. If we don't do this
-            # then a model trained with deep_supervision=True could not easily be loaded at inference time where
-            # deep supervision is not needed. It's just a convenience thing
-            self.seg_layers.append(
-                encoder.conv_op(input_features_skip, num_classes, 1, 1, 0, bias=True)
-            )
 
-    def forward(self, x):
-        features = []
-        for i, stage in enumerate(self.stages):
-            x = stage(x)
-            features.append(x)
-            if i < len(self.down_blocks):
-                x = self.down_blocks[i](x)
-        return x, features
+        # Final output mask
+        self.seg_layers.append(
+            encoder.conv_op(
+                encoder.output_channels[0],
+                num_classes,
+                kernel_size=1,
+                stride=1,
+                padding=0,
+                bias=True,
+            )
+        )
+
+        self.n_blocks_per_stage = n_blocks_per_stage
+        self.exp_ratio_per_stage = exp_ratio_per_stage
+
+    def forward(self, skips):
+        """
+        we expect to get the skips in the order they were computed, so the bottleneck should be the last entry
+        """
+        x = skips[-1]
+        seg_outputs = []
+        for s in range(len(self.stages)):
+            if self.deep_supervision:
+                seg_outputs.append(self.seg_layers[s](x))
+
+            x_up = self.up_blocks[s](x)
+            x_skip = skips[-(s + 1)]
+            x = x_up + x_skip
+            x = self.stages[s](x)
+
+        final_output = self.seg_layers[-1](x)
+        if not self.deep_supervision:
+            return final_output
+        else:
+            seg_outputs.append(final_output)
+            # invert seg outputs so that the largest segmentation prediction is returned first
+            seg_outputs = seg_outputs[::-1]
+            return seg_outputs
 
 
 if __name__ == "__main__":
